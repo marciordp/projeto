83fbdf894abdc9033b54f6400a87f6735bf72a8c
marcio <marciosadns@gmail.com>
2025-06-22 22:52:25 -0300

    teste logo claro

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/logotipo-claro.png")
+  } else {
+    img.setAttribute("src", "./assets/logo-bem-claro.png")
+  }
 }
